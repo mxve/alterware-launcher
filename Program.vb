e5c1059bb57c1e2dc7490cb63c8440ebd5dfcb3f
mxve <68632137+mxve@users.noreply.github.com>
2023-06-01 12:23:23 +0200

    initial implementation

--- Program.vb
+++ Program.vb
@@ -1,7 +1,39 @@
 Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Net.Http
 
 Module Program
+    Private master As String = "https://master.alterware.dev"
+
+    Sub download_and_run(game As String)
+        Dim filename As String = game & ".exe"
+        Dim remote_path As String = If(game = "iw4-sp", "/iw4/", "/iw5/")
+        Console.WriteLine("Downloading " & game & "...")
+        Using wc As New WebClient
+            wc.DownloadFile(master & remote_path & filename, filename)
+        End Using
+        Console.WriteLine("Starting " & game)
+        Process.Start(filename)
+        End
+    End Sub
+
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim game As String
+        Try
+            game = args(0)
+        Catch ex As Exception
+            If File.Exists("iw4sp.exe") Or File.Exists("iw4mp.exe") Then
+                game = "iw4-sp"
+            ElseIf File.Exists("iw5sp.exe") Or File.Exists("iw5mp.exe") Or File.Exists("iw5mp_server.exe") Then
+                game = "iw5-mod"
+            Else
+                Console.WriteLine("No game specified nor found in local directory")
+                Console.ReadLine()
+                Return
+            End If
+        End Try
+
+        download_and_run(game)
     End Sub
 End Module
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # alterware-launcher
+
+1. Download [latest release](https://github.com/mxve/alterware-launcher/releases/latest/download/alterware-launcher.exe)
+2. Please alterware-launcher.exe in MW2/MW3 game directory
+3. Run alterware-launcher.exe
+
+If the launcher doesn't work make sure you have dotnet 6.0 installed. Also try running the launcher as administrator.
