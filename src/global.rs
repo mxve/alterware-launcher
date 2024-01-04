1147e0673b55dbf66bfe64a120700e499eda99ba
mxve <68632137+mxve@users.noreply.github.com>
2024-01-04 18:53:44 +0100

    use new cdn

--- src/global.rs
+++ src/global.rs
@@ -1,4 +1,4 @@
-pub const MASTER: &str = "https://master.alterware.dev";
+pub const MASTER: &str = "http://cdn.alterware.ovh";
 pub const GH_OWNER: &str = "mxve";
 pub const GH_REPO: &str = "alterware-launcher";
 pub const GH_IW4X_OWNER: &str = "iw4x";
