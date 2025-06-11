fdd8c9ea9b64f4ac8b1742d04a77201e53316c92
alterware <info@alterware.dev>
2025-06-11 08:46:53 +0200

    v0.10.9

--- src/global.rs
+++ src/global.rs
@@ -8,7 +8,7 @@ use std::sync::Mutex;
 
 use crate::cdn::{Hosts, Region, Server};
 
-pub const GH_OWNER: &str = "mxve";
+pub const GH_OWNER: &str = "alterware";
 pub const GH_REPO: &str = "alterware-launcher";
 pub const GH_IW4X_OWNER: &str = "iw4x";
 pub const GH_IW4X_REPO: &str = "iw4x-client";
