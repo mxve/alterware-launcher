af672e1f092ee7bbf7eac8baad64bd18d306437d
mxve <68632137+mxve@users.noreply.github.com>
2025-06-12 18:08:20 +0200

    clean

--- res/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        static_vcruntime::metabuild();
-        let mut res = winresource::WindowsResource::new();
-        res.set_icon("res/icon.ico").set_language(0x0409);
-        res.compile().unwrap();
-    }
-}
